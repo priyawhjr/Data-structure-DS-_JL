aaad163495572f0edffed9c5ca2c95a98d5745e1
priyawhjr <132220332+priyawhjr@users.noreply.github.com>
2026-02-12 02:20:23 +0530

    Add files via upload

---
+++ L4SearchH.w.py
@@ -0,0 +1,32 @@
+
+
+#Practice 1: The "Contact Finder" (Linear Search)
+
+#Task: Create a program that searches for 
+# a specific name in a list of contacts.
+
+#nput: Create a list contacts = ["Alice", "Bob", "Charlie", "David", "Eve"].
+
+'''User Input: Ask the user "Who are you looking for?".
+Logic (Linear) Hint (Give if needed):
+Loop through the list from start to end.
+If contacts[i] == user_input, print "Found [Name] at index [i]".
+If the loop finishes without finding it, print "Contact not found".'''
+
+# Predefined contact list
+contacts = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+# Take user input
+name = input("Who are you looking for? ")
+
+found = False
+
+# Linear Search
+for i in range(len(contacts)):
+    if contacts[i].lower() == name.lower():
+        print(f"Found {contacts[i]} at index {i}")
+        found = True
+        break
+
+if not found:
+    print("Contact not found")
